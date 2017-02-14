87c373dc92e2a251f4ec52c57dc32c95c548e0c9
shreyaTrivedi <shreya0trivedi@gmail.com>
2017-02-14 18:10:12 +0530

    changes parameters of Amax.py

--- Amax.py
+++ Amax.py
@@ -5,12 +5,14 @@ and returns the largest of them. Use the if-then-else construct
 available in Python. (It is true that Python has the max() function
 built in, but writing it yourself is nevertheless a good exercise.)
 '''
+import sys
+
 def max_num(num1, num2):
     if num1 > num2:
-        print "num1 %d is greater than num2 %d" % (num1, num2)
+        print "num1 %r is greater than num2 %r" % (num1, num2)
     elif num1 < num2:
-        print "num2 %d is greater than num1 %d" % (num2, num1)
+        print "num2 %r is greater than num1 %r" % (num2, num1)
     else:
         print "num1 and num2 are equal"
 
-max_num(4, 3)
+max_num(sys.argv[1], sys.argv[2])
