7d5c878eea3c725890ed74017f91eda552922556
shreyaTrivedi <shreya0trivedi@gmail.com>
2017-02-15 11:52:15 +0530

    changed parameters

--- q03lenght.py
+++ q03lenght.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3.5
 
-''' 
+'''
 Problem Statement
 Define a function that computes the length of a given list or string.
 (It is true that Python has the len() function built in,
 but writing it yourself is nevertheless a good exercise.)
 '''
 
+import sys
+
 def get_lenght(word):
+    '''  Function to get lenght of a string'''
     count = 0
     for char in word:
         count += 1
     return count
 
-print(get_lenght('abhi'))
-
+print get_lenght(sys.argv[1])
