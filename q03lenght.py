f266061ae19b39c40e71b1ca6395f61660fb6833
shreyaTrivedi <shreya0trivedi@gmail.com>
2017-02-15 11:55:12 +0530

    added docstring

--- q03lenght.py
+++ q03lenght.py
@@ -10,7 +10,12 @@ but writing it yourself is nevertheless a good exercise.)
 import sys
 
 def get_lenght(word):
-    '''  Function to get lenght of a string'''
+    '''  Function to get lenght of a string
+    Args:
+        word = The only parameter.
+    Returns:
+        count = lenght of string.
+    '''
     count = 0
     for char in word:
         count += 1
