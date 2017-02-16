c03cf7dd147cb5d192b21621647e135436483f5c
shreyaTrivedi <shreya0trivedi@gmail.com>
2017-02-16 16:51:03 +0530

    pylint qualified

--- q05translate.py
+++ q05translate.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3.5
 
-'''Write a function translate() that will translate a text into 
+'''Write a function translate() that will translate a text into
 "rvarsprket" (Swedish for "robber's language"). That is,
 double every consonant and place an occurrence of "o" in between.
-For example, translate("this is fun")should return the string 
+For example, translate("this is fun")should return the string
 "tothohisos isos fofunon".'''
 
 import sys
 
 def translate(str1):
-    p = ""
+    '''Function to translate string.
+    Args
+        str1 : String to be converted.
+    Returns
+        Converted string.'''
+
+    str2 = ""
     for char in str1:
         if char not in "aieou ":
             str1 = char + 'o' + char
-            p += str1
+            str2 += str1
         else:
             str1 = char
-            p += char
-    print p
+            str2 += char
+    print str2
 
 translate(sys.argv[1])
