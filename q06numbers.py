e593a025622b5c8f6d8e210720bedf890d3ccaf2
shreyaTrivedi <shreya0trivedi@gmail.com>
2017-02-17 12:37:11 +0530

    q06numbers.py pylint test done

--- q06numbers.py
+++ q06numbers.py
@@ -5,19 +5,31 @@ and multiplies (respectively) all the numbers in a list of numbers.
 For example, sum([1, 2, 3, 4]) should return 10,
 and multiply([1, 2, 3, 4]) should return 24.'''
 
-numbers = [1, 2, 3, 4]
+nums = input("Enter in a list(comma saperated within square braces): ")
 
-def sumnum(numbers):
+def sumnum(nums):
+    ''' Function to get the sum of numbers.
+    Args:
+        nums: A list of numbers which are to be added.
+    result:
+        total: sum of the numbers.
+    '''
     total = 0
-    for n in numbers:
-        total += n 
+    for n in nums:
+        total += n
     print "addition is ", total
 
-def multiply(numbers):
+def multiply(nums):
+    '''Fuction ti get the multiplication of the numbers.
+    Args:
+        nums: A list of numbers which are to be multiplied.
+    result:
+        total: multiplication of the numbers.
+    '''
     total = 1
-    for n in numbers:
+    for n in nums:
         total *= n
     print "multiplication is ", total
 
-sumnum(numbers)
-multiply(numbers)
+sumnum(nums)
+multiply(nums)
